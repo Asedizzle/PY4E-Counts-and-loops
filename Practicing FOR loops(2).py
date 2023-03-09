943ebc97c6652b74554e31a75e1d4da710c08d75
Asedizzle <125497646+Asedizzle@users.noreply.github.com>
2023-03-09 04:03:33 -0800

    Add files via upload

---
+++ Practicing FOR loops(2).py
@@ -0,0 +1,19 @@
+x = [1,2,3]
+for i in x:
+    print(i)
+    
+for i in range(1,25,2):
+    print(i)
+
+for i in range(1,10,-1):
+    print(i)
+    
+for i in(30,15,-2):
+    print(i)
+     
+x = int(input("How many drinks do you want"))
+i=1
+while i<=x:
+    print("drink")
+    i+=1
+
